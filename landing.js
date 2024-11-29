192996b5cbc889a72bef61d622c1b147821341d1
Gayathrinaidu88 <240768.AD@rmkec.ac.in>
2024-11-29 19:44:53 +0530

    Create landing.js

---
+++ landing.js
@@ -0,0 +1,4 @@
+// JavaScript for button interaction
+document.getElementById("ctaButton").addEventListener("click", function() {
+    alert("Thank you for clicking! We will redirect you soon.");
+});
